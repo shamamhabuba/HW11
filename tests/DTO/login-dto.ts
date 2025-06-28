f9afe33030db7509fe884ba3313717c236fb1cdc
mypc <shamamhabuba@gmail.com>
2025-06-28 21:34:23 +0300

    HW11 authentification test

---
+++ tests/DTO/login-dto.ts
@@ -0,0 +1,16 @@
+export class LoginDto {
+  username: string;
+  password: string;
+
+   constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+
+  static loginWithCorrectData(): LoginDto {
+    const username = process.env.USER || '';
+    const password = process.env.PASSWORD || '';
+    return new LoginDto(username, password);
+  }
+}
+
